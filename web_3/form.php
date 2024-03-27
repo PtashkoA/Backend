3c676c5be75084f40447c989de4e745fcb60a818
PtashkoA <144013075+PtashkoA@users.noreply.github.com>
2024-03-27 20:10:14 +0300

    form.php

--- web_3/form.php
+++ web_3/form.php
@@ -1,40 +1,5 @@
 <?php
 
-header('Content-Type: text/html; charset=UTF-8');
-
-if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-  if (!empty($_GET['save'])) {
-    print('Спасибо, результаты сохранены.');
-  }
-  include('form.php');
-  exit();
-}
-
-$errors = FALSE;
-if (empty($_POST['fio'])) {
-  print('Заполните ФИО.<br/>');
-  $errors = TRUE;
-}
-
-if (empty($_POST['email']) || !is_numeric($_POST['email']) || !preg_match('/^\d+$/', $_POST['email'])) {
-  print('Заполните E-mail.<br/>');
-  $errors = TRUE;
-}
-
-if (empty($_POST['tel']) || !is_numeric($_POST['tel']) || !preg_match('/^\d+$/', $_POST['tel'])) {
-  print('Заполните номер телефона.<br/>');
-  $errors = TRUE;
-}
-
-if (empty($_POST['date_of_birth']) || !is_numeric($_POST['date_of_birth']) || !preg_match('/^\d+$/', $_POST['date_of_birth'])) {
-  print('Заполните дату рождения.<br/>');
-  $errors = TRUE;
-}
-
-if ($errors) {
-  exit();
-}
-
 $user = 'u67326';
 $pass = '6963806';
 $db = new PDO('mysql:host=localhost;dbname=u67326', $user, $pass, [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
