ce67df983b5a5e23300fc7524ffddedc3d1b3c81
PtashkoA <144013075+PtashkoA@users.noreply.github.com>
2024-05-20 17:47:36 +0300

    Update index.php

--- web_3/index.php
+++ web_3/index.php
@@ -48,8 +48,8 @@ if (empty($_POST['radio1'])) {
 
 include('../password.php');
     if (!empty($_COOKIE[session_name()]) &&
-    session_start() && !empty($_SESSION['$user'])){
-        $userLogin = $_SESSION['$user'];
+    session_start() && !empty($_SESSION['login'])){
+        $userLogin = $_SESSION['login'];
     }
 $db = new PDO('mysql:host=localhost;dbname=u67326', $user, $pass,
   [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]); 
